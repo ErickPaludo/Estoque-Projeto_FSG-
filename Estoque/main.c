bdc1d219adf3b1fa86fbc101d7a6673ff952507b
Erick <erickpaludoyt@gmail.com>
2023-04-23 23:13:00 -0300

    23/04.1

--- Estoque/main.c
+++ Estoque/main.c
@@ -1,16 +1,28 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 int main()
 {
-    char user;
+    char user[10];
     int pass;
-    int verfificator;
+    int vrfc;
 
-   do
-   {
-       printf("User: ");
-       scanf("%s", &user);
-   }while(user == 'ADM' || user != 'UserD');
+while(vrfc == 0)
+{
+     printf("User: ");
+     scanf("%s", &user);
+
+    if (strcmp(user, "ADM") == 0 || strcmp(user, "empeloyee") == 0)
+    {
+    printf("Password: ");
+    scanf("%d", &pass);
+    }
+    else
+    {
+        system("CLS");
+    }
+
+}
     return 0;
 }
