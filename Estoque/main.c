5fb335333285617a535f2638b703a5e5349df02f
Erick <erickpaludoyt@gmail.com>
2023-04-24 22:39:01 -0300

    24/04

--- Estoque/main.c
+++ Estoque/main.c
@@ -6,23 +6,65 @@ int main()
 {
     char user[10];
     int pass;
-    int vrfc;
+    int vrfc, cont_0 = 0;
 
-while(vrfc == 0)
+while(vrfc == 0) // cadeia de repetição
 {
-     printf("User: ");
+     printf("User: "); //solicitar User ADM
      scanf("%s", &user);
 
-    if (strcmp(user, "ADM") == 0 || strcmp(user, "empeloyee") == 0)
-    {
-    printf("Password: ");
-    scanf("%d", &pass);
-    }
-    else
-    {
-        system("CLS");
+     //LOGIN ADM ------------------------------------------------
+
+    if (strcmp(user, "ADM") == 0)
+        {
+            do
+            {
+                system("Cls");
+                printf("User: %s", user);
+                printf("\nPassword: "); //solicita senha ADM
+                scanf("%d", &pass);
+
+                cont_0++;
+                if(cont_0 == 3)
+                {
+                    exit(0);
+                }
+
+
+            } while (pass != 409027);
+             vrfc = 1;
+        }
+         //lOGIN FUNCIONRIO ------------------------------------------------
+
+       else if (strcmp(user, "EMP") == 0)
+        {
+            do
+            {
+                system("Cls");
+                printf("User: %s", user);
+                printf("\nPassword: "); //solicita senha ADM
+                scanf("%d", &pass);
+
+            } while (pass != 300468);
+             vrfc = 1;
+        }
+
+        //User INVALID
+
+        else
+        {
+            system("CLS");
+        }
     }
 
-}
+    //NOVA ÀREA
+
+    printf("\033[32mACESSADO\033[0m\n");
+
+    if (strcmp(user, "ADM") == 0)
+        {
+
+        }
+
     return 0;
 }
