06fe14135284d79e68b87df7a031202e9e7d67e2
Erick <erickpaludoyt@gmail.com>
2023-04-26 19:34:43 -0300

    26/04

--- Estoque/main.c
+++ Estoque/main.c
@@ -5,11 +5,13 @@
 int main()
 {
     char user[10];
-    int pass;
-    int vrfc, cont_0 = 0;
+    int pass, name_arq, op;
+    int vrfc = 0, cont_0 = 0;
+
 
 while(vrfc == 0) // cadeia de repetição
 {
+    system("Cls");
      printf("User: "); //solicitar User ADM
      scanf("%s", &user);
 
@@ -17,17 +19,26 @@ while(vrfc == 0) // cadeia de repetição
 
     if (strcmp(user, "ADM") == 0)
         {
+            system("Cls");
             do
             {
-                system("Cls");
+
                 printf("User: %s", user);
                 printf("\nPassword: "); //solicita senha ADM
                 scanf("%d", &pass);
-
                 cont_0++;
+                system("Cls");
+
+                if(cont_0 <= 3)
+                {
+                    printf("Tentativa %d/3 \n", cont_0);
+                }
+
                 if(cont_0 == 3)
                 {
+                    printf("Voce execeu o numero de tentativas, tente novamente mais tarde! \n", cont_0);
                     exit(0);
+
                 }
 
 
@@ -38,12 +49,27 @@ while(vrfc == 0) // cadeia de repetição
 
        else if (strcmp(user, "EMP") == 0)
         {
+            system("Cls");
             do
             {
-                system("Cls");
+
                 printf("User: %s", user);
                 printf("\nPassword: "); //solicita senha ADM
                 scanf("%d", &pass);
+                system("Cls");
+                cont_0++;
+
+                if(cont_0 <= 3)
+                {
+                    printf("Tentativa %d/3 \n", cont_0);
+                }
+
+                if(cont_0 == 3)
+                {
+                    printf("Voce execeu o numero de tentativas, tente novamente mais tarde! \n", cont_0);
+                    exit(0);
+
+                }
 
             } while (pass != 300468);
              vrfc = 1;
@@ -53,16 +79,43 @@ while(vrfc == 0) // cadeia de repetição
 
         else
         {
-            system("CLS");
+
         }
     }
 
-    //NOVA ÀREA
-
+    //ACESSO
+    system("CLS");
     printf("\033[32mACESSADO\033[0m\n");
+
+    //ABA ORIGINAL ADM
+
+
+     for(int i = 0; i <= 100;i++)
+     {
+
+    char nome_arquivo[10];
+        sprintf(nome_arquivo, "Nome%d.txt", i);
+        FILE *fp;
+        fp = fopen(nome_arquivo, "w");
+        fprintf(fp, " NULL ");
+        fclose(fp);
+
+        char preco_arquivo[10];
+        sprintf(preco_arquivo, "Preco%d.txt", i);
+        fp = fopen(preco_arquivo, "w");
+        fprintf(fp, "NHE ");
+        fclose(fp);
+     }
+
 
     if (strcmp(user, "ADM") == 0)
         {
+            //Home
+            printf("\nADD estoque: (1)");
+            printf("\nRemove estoque: (2)");
+            printf("\nAdd no estoque (3)");
+            printf("\nSaida do estoque (4)");
+            scanf("%d", &op);
 
         }
 
